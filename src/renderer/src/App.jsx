9ef870047616732e4a1a76f99cec2557a150e214
TwAzuremy <958559114@qq.com>
2024-12-21 23:38:33 +0800

    Organize the project structure

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,13 +1,17 @@
-import { headingsPlugin, MDXEditor } from '@mdxeditor/editor';
+import './assets/css/main.scss';
+
+// import { headingsPlugin, MDXEditor } from '@mdxeditor/editor';
 import '@mdxeditor/editor/style.css';
+import AppHeader from './pages/Header';
 
 function App() {
     return (
         <div id="app">
-            <MDXEditor markdown={"# Hello World"} plugins={[headingsPlugin()]} />
+            {/* <MDXEditor markdown={"# Hello World"} plugins={[headingsPlugin()]} /> */}
+            <AppHeader />
         </div>
     );
 }
 
-export default App
+export default App;
 
---
+++ src/renderer/src/pages/Header.jsx
@@ -0,0 +1,9 @@
+import "./assets/css/appHeader.scss";
+
+function AppHeader() {
+    return (
+        <header id="app__header"></header>
+    );
+}
+
+export default AppHeader;

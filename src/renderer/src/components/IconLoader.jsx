bc0cd81b5d9c89a37bd990f19002f583d976ac19
TwAzuremy <958559114@qq.com>
2025-03-04 00:07:54 +0800

    refactor (doc comments):
    - Add comments to the IconLoader and Button components.
    - Add an ignore rule for .gitignore

--- src/renderer/src/components/IconLoader.jsx
+++ src/renderer/src/components/IconLoader.jsx
@@ -1,5 +1,18 @@
 import {lazy, Suspense} from "react";
 
+/**
+ * A function component that loads an SVG icon by name.
+ *
+ * @function IconLoader
+ *
+ * @param {string} name The name of the icon to load.
+ * @param {*} [props] Any additional props to be passed to the icon component.
+ *
+ * @returns {ReactElement} A React element representing the loaded icon component.
+ *
+ * @example
+ * <IconLoader name={"icon-name"}/>
+ */
 const IconLoader = ({name, ...props}) => {
     const Icon = lazy(() => import(`@assets/icons/${name}.svg?react`));
 

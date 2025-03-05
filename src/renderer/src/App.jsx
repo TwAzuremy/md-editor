000f58b09696ba877f5df2d289bbfa2a55783c33
TwAzuremy <958559114@qq.com>
2025-03-05 20:49:15 +0800

    refactor (sidebar wrapper): Embed the file into App.jsx.

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -3,7 +3,36 @@ import "@assets/css/app.scss";
 import MDEHeader from "@renderer/container/MDEHeader.jsx";
 import MDEContent from "@renderer/container/MDEContent.jsx";
 import WindowDrag from "@components/WindowDrag.jsx";
-import MDESidebarWrapper from "@renderer/container/wrapper/MDESidebarWrapper.jsx";
+import MDESideNavBar from "@renderer/container/MDESideNavBar.jsx";
+import MDESidebar from "@renderer/container/MDESidebar.jsx";
+import IconLoader from "@components/IconLoader.jsx";
+import MDEFileManager from "@renderer/container/sidebar/MDEFileManager.jsx";
+
+import {useState} from "react";
+
+/**
+ * @prop {string} id Id of the nav bar and sidebar page switch.
+ * @prop {string} tip The tooltip of the nav bar.
+ * @prop {ReactElement} icon The icon of the nav bar.
+ * @prop {ReactElement} component The component of the sidebar.
+ *
+ * @type {[{id: string, tip: string, icon: ReactElement, component: ReactElement}]}
+ */
+const sidebarList = [
+    {
+        "id": "file-manager",
+        "tip": "File Manager",
+        "icon": <IconLoader name={"file-manager"}/>,
+        "component": <MDEFileManager/>
+    },
+    // TODO: [Deleted] Here is the test case.
+    {
+        "id": "test",
+        "tip": "Test",
+        "icon": <IconLoader name={"settings"}/>,
+        "component": <span>Test Page</span>
+    }
+];
 
 function App() {
     return (
@@ -13,9 +42,21 @@ function App() {
             <MDEHeader/>
             {/* Window drag area used for the sidebar */}
             <WindowDrag direction={"vertical"}/>
-            <MDESidebarWrapper />
+            <SidebarWrapper />
             <MDEContent/>
         </div>
+    );
+}
+
+function SidebarWrapper() {
+    const [renderId, setRenderId] = useState(null);
+
+    // noinspection JSValidateTypes
+    return (
+        <>
+            <MDESideNavBar navClickEvent={setRenderId} navList={sidebarList}/>
+            <MDESidebar renderId={renderId} pageList={sidebarList}/>
+        </>
     );
 }
 

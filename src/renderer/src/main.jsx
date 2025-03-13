a1c935e287abd0bc2bd3358f7d6df4ffc70a5c99
TwAzuremy <958559114@qq.com>
2025-03-13 20:38:38 +0800

    chore: react-scan

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -4,6 +4,14 @@ import App from "./App";
 import "@assets/css/base.scss";
 import store from "@store/store";
 import {Provider} from "react-redux";
+import {scan} from "react-scan";
+
+scan({
+    enabled: true,
+    log: false,
+    playSound: false,
+    showToolbar: true
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
